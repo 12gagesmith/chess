bc951c8b605b045c28da800c7b149b72a10e15f9
Gage Smith <gagesmith242@gmail.com>
2025-01-22 22:00:11 -0700

    Mostly finished Rook move calculator

--- shared/src/main/java/chess/MoveCalculator.java
+++ shared/src/main/java/chess/MoveCalculator.java
@@ -11,17 +11,54 @@ public class MoveCalculator {
         return false;
     }
 
+    private static boolean inBounds(ChessPosition position) {
+        return position.getRow() >= 1 && position.getRow() <= 8 && position.getColumn() >= 1 && position.getColumn() <= 8;
+    }
+
     private static boolean isSquareOpen(ChessBoard board, ChessPosition position, ChessPiece myPiece) {
-        if (position.getRow() < 1 || position.getRow() > 8 || position.getColumn() < 1 || position.getColumn() > 8) {
+        if (!inBounds(position)) {
             return false;
         }
         ChessPiece otherPiece = board.getPiece(position);
         return otherPiece == null || canCapture(myPiece, otherPiece);
     }
 
+    private static ArrayList<ChessMove> goStraight(ChessBoard board, ChessPiece myPiece, ChessPosition myPosition, ArrayList<ChessMove> moves, String direction) {
+        ChessPosition nextPosition = new ChessPosition(myPosition.getRow(), myPosition.getColumn());
+        switch (direction) {
+            case "up" -> {
+                do {
+                    nextPosition = new ChessPosition(nextPosition.getRow() + 1, nextPosition.getColumn());
+                    if (isSquareOpen(board, nextPosition, myPiece)) {moves.add(new ChessMove(myPosition, nextPosition, null));}
+                } while (inBounds(nextPosition) && board.getPiece(nextPosition) == null);
+            }
+            case "down" -> {
+                do {
+                    nextPosition = new ChessPosition(nextPosition.getRow() - 1, nextPosition.getColumn());
+                    if (isSquareOpen(board, nextPosition, myPiece)) {moves.add(new ChessMove(myPosition, nextPosition, null));}
+                } while (inBounds(nextPosition) && board.getPiece(nextPosition) == null);
+            }
+            case "left" -> {
+                do {
+                    nextPosition = new ChessPosition(nextPosition.getRow(), nextPosition.getColumn() - 1);
+                    if (isSquareOpen(board, nextPosition, myPiece)) {moves.add(new ChessMove(myPosition, nextPosition, null));}
+                } while (inBounds(nextPosition) && board.getPiece(nextPosition) == null);
+            }
+            case "right" -> {
+                do {
+                    nextPosition = new ChessPosition(nextPosition.getRow(), nextPosition.getColumn() + 1);
+                    if (isSquareOpen(board, nextPosition, myPiece)) {moves.add(new ChessMove(myPosition, nextPosition, null));}
+                } while (inBounds(nextPosition) && board.getPiece(nextPosition) == null);
+            }
+        }
+        return moves;
+    }
+
     public static ArrayList<ChessMove> calculateKing(ChessBoard board, ChessPiece myPiece, ChessPosition myPosition, ArrayList<ChessMove> moves) {
+        // These two lines get the next position, check if that square's open, then adds it to moves if it is.
         ChessPosition nextPosition = new ChessPosition(myPosition.getRow() - 1, myPosition.getColumn());
         if (isSquareOpen(board, nextPosition, myPiece)) {moves.add(new ChessMove(myPosition, nextPosition, null));}
+        // Repeat 7 more times
         nextPosition = new ChessPosition(myPosition.getRow() - 1, myPosition.getColumn() + 1);
         if (isSquareOpen(board, nextPosition, myPiece)) {moves.add(new ChessMove(myPosition, nextPosition, null));}
         nextPosition = new ChessPosition(myPosition.getRow() - 1, myPosition.getColumn() - 1);
@@ -48,8 +85,10 @@ public class MoveCalculator {
     }
 
     public static ArrayList<ChessMove> calculateKnight(ChessBoard board, ChessPiece myPiece, ChessPosition myPosition, ArrayList<ChessMove> moves) {
+        // These two lines get the next position, check if that square's open, then adds it to moves if it is.
         ChessPosition nextPosition = new ChessPosition(myPosition.getRow() - 1, myPosition.getColumn() - 2);
         if (isSquareOpen(board, nextPosition, myPiece)) {moves.add(new ChessMove(myPosition, nextPosition, null));}
+        // Repeat 7 more times
         nextPosition = new ChessPosition(myPosition.getRow() - 1, myPosition.getColumn() + 2);
         if (isSquareOpen(board, nextPosition, myPiece)) {moves.add(new ChessMove(myPosition, nextPosition, null));}
         nextPosition = new ChessPosition(myPosition.getRow() + 1, myPosition.getColumn() - 2);
@@ -68,6 +107,10 @@ public class MoveCalculator {
     }
 
     public static ArrayList<ChessMove> calculateRook(ChessBoard board, ChessPiece myPiece, ChessPosition myPosition, ArrayList<ChessMove> moves) {
+        moves = goStraight(board, myPiece, myPosition, moves, "up");
+        moves = goStraight(board, myPiece, myPosition, moves, "down");
+        moves = goStraight(board, myPiece, myPosition, moves, "left");
+        moves = goStraight(board, myPiece, myPosition, moves, "right");
         return moves;
     }
 
