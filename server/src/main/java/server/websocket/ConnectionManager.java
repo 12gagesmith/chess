a2d007e7b888f8ded8bc2b413200e81b1d9745ff
Gage Smith <gagesmith242@gmail.com>
2025-04-09 16:15:23 -0600

    Started filling out methods in WebsocketHandler, ConnectionManager, and Connection

--- server/src/main/java/server/websocket/ConnectionManager.java
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -1,4 +1,38 @@
 package server.websocket;
 
+import org.eclipse.jetty.websocket.api.Session;
+import websocket.messages.ServerMessage;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class ConnectionManager {
+    public final ConcurrentHashMap<String, Connection> connections = new ConcurrentHashMap<>();
+
+    public void add(String authToken, Session session) {
+        Connection connection = new Connection(authToken, session);
+        connections.put(authToken, connection);
+    }
+
+    public void remove(String authToken) {
+        connections.remove(authToken);
+    }
+
+    public void broadcast(String excludeAuthToken, ServerMessage serverMessage) throws IOException {
+        ArrayList<Connection> removeList = new ArrayList<>();
+        for (Connection c : connections.values()) {
+            if (c.session.isOpen()) {
+                if (!c.authToken.equals(excludeAuthToken)) {
+                    c.send(serverMessage.toString());
+                }
+            } else {
+                removeList.add(c);
+            }
+        }
+
+        for (Connection c : removeList) {
+            connections.remove(c.authToken);
+        }
+    }
 }
--- server/src/main/java/server/websocket/WebsocketHandler.java
+++ server/src/main/java/server/websocket/WebsocketHandler.java
@@ -1,4 +1,34 @@
 package server.websocket;
 
+import com.google.gson.Gson;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import websocket.commands.UserGameCommand;
+
+import java.io.IOException;
+
+@WebSocket
 public class WebsocketHandler {
+
+    private final ConnectionManager connections = new ConnectionManager();
+
+    @OnWebSocketMessage
+    public void onMessage(Session session, String message) throws IOException {
+        UserGameCommand userGameCommand = new Gson().fromJson(message, UserGameCommand.class);
+        switch (userGameCommand.getCommandType()) {
+            case CONNECT -> connect(userGameCommand.getAuthToken(), session);
+            case MAKE_MOVE -> make_move();
+            case LEAVE -> leave();
+            case RESIGN -> resign();
+        }
+    }
+
+    private void connect(String authToken, Session session) {}
+
+    private void make_move() {}
+
+    private void leave() {}
+
+    private void resign() {}
 }
