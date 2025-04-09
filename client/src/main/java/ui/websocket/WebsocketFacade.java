3961f3817568b3522904e3f8a1c1321fd8a0e85f
Gage Smith <gagesmith242@gmail.com>
2025-04-09 17:21:06 -0600

    Started working on WebSocketFacade

--- client/src/main/java/ui/websocket/WebsocketFacade.java
+++ client/src/main/java/ui/websocket/WebsocketFacade.java
@@ -1,4 +1,41 @@
 package ui.websocket;
 
-public class WebsocketFacade {
+import com.google.gson.Gson;
+import serverfacade.DataAccessException;
+import websocket.messages.ServerMessage;
+
+import javax.websocket.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class WebsocketFacade extends Endpoint {
+
+    Session session;
+    NotificationHandler notificationHandler;
+
+    public WebsocketFacade(String url, NotificationHandler notificationHandler) throws DataAccessException {
+        try {
+            url = url.replace("http", "ws");
+            URI socketURI = new URI(url + "/ws");
+            this.notificationHandler = notificationHandler;
+
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            this.session = container.connectToServer(this, socketURI);
+
+            this.session.addMessageHandler(new MessageHandler.Whole<String>() {
+                @Override
+                public void onMessage(String message) {
+                    ServerMessage serverMessage = new Gson().fromJson(message, ServerMessage.class);
+                    notificationHandler.notify(serverMessage);
+                }
+            });
+        } catch (URISyntaxException | IOException | DeploymentException ex) {
+            throw new DataAccessException(500, ex.getMessage());
+        }
+    }
+
+    @Override
+    public void onOpen(Session session, EndpointConfig endpointConfig) {
+    }
 }
